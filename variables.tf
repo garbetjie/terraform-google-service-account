9ab95862fda7b2aaf7f023770f72cab877ffe9ed
Geoff Garbers <geoff@garbers.co.za>
2019-05-21 06:49:38 +0200

    Add descriptions to variables.

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable roles {
   type = "list"
+  description = "A list of roles to assign to the service account"
 }
 
 variable account_id {
   type = "string"
+  description = "The unique ID of the service account, used to create the service account's email address"
 }
 
 variable display_name {
   type = "string"
   default = ""
+  description = "Display name of the service account to show"
 }
 
 variable display_name_suffix {
   type = "string"
   default = " (managed by terraform)"
+  description = "String to append to the display name"
 }
 
 locals {
